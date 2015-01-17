1fced028ffd8bef53ff6f04225e00bbb985b12a4
Rudi Grinberg <rudi.grinberg@gmail.com>
2015-01-17 18:32:23 -0500

    initial

---
+++ lib/mix/tasks/ctags.ex
@@ -0,0 +1,24 @@
+defmodule Mix.Tasks.Ctags do
+  use Mix.Task
+
+  defp ctags_opt_path do
+    base = "lib/ctags_ex"
+    p1 = "#{Mix.Project.deps_path}/mix_ctags/#{base}"
+    p2 = "./#{base}"
+
+    cond do
+      File.exists? p1 -> {:ok, p1}
+      File.exists? p2 -> {:ok, p2}
+      true            -> :not_found
+    end
+  end
+
+  @shortdoc "Generate ctags for the project"
+  def run(args) do
+    case ctags_opt_path do
+      {:ok, p}   -> Mix.Shell.IO.cmd "ctags --options='./lib/ctags_ex' -R ."
+      :not_found -> IO.puts "Cannot find ctags_ex file"
+    end
+  end
+
+end
---
+++ mix.exs
@@ -0,0 +1,14 @@
+defmodule MixCtags.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :mix_ctags,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     deps: deps]
+  end
+
+  defp deps do
+    []
+  end
+end
